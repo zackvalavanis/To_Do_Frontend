be9753bfb11ebdc84461c4530f99ceea176bd477
Zack <168221270+Zval321@users.noreply.github.com>
2024-11-06 20:58:17 -0600

    index page for to do list

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,37 @@
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom';
 
 export function HomePage () { 
 
   const activities = useLoaderData();
+  
+  const formatTime = (time) => {
+    if (!time || typeof time !== 'string') {
+      return "Invalid Time"; 
+    }
+
+    const date = new Date(time);
+    if (isNaN(date.getTime())) {
+      return "Invalid Time";  
+    }
+
+    return date.toLocaleTimeString();  
+  };
 
   return ( 
     <div>
-    
+      {activities.map((activity) => {
+        return (
+          <div key={activity.id}>
+            <h1>{activity.name}</h1>
+            <p>{activity.date}</p>
+              {activity.categories && activity.categories.map((category, index) => ( 
+                <span key={index}>{category.category_type}</span> 
+              ))}
+            <p>{formatTime(activity.time_start)}</p>
+            <p>{formatTime(activity.time_end)}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
