013e5826a6a08d9a813dbfb0ae60cbadf8b218f8
Zack <168221270+Zval321@users.noreply.github.com>
2024-11-17 12:52:29 -0600

    added navigate to logout function

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -2,9 +2,11 @@ import axios from 'axios'
 import { AuthContext } from './AuthContext';
 import React, { useContext} from 'react';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom'
 
 export function Logout({ className }) { 
   const { setCurrentUser, setIsLoggedIn, logout} = useContext(AuthContext);
+  const navigate = useNavigate()
 
   const handleClick = (event) => { 
     event.preventDefault();
@@ -13,6 +15,7 @@ export function Logout({ className }) {
     setCurrentUser(null);
     setIsLoggedIn(false);
     toast.success('User successfully Logged Out.')
+    navigate('/');
     console.log('You have successfully Logged Off')
   }
   return ( 
